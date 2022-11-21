687a6e8554f65b63cbc79cdab67df59e7abfe1d1
lalankumar001 <103441165+lalankumar001@users.noreply.github.com>
2022-11-21 13:59:00 +0530

    updatenavbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">What we do</Link>
         </li>
         <li>
           <Link to="/projects">Projects</Link>
